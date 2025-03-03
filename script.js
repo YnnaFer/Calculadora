883b3d46c4a754e01b3bf5a1a61b8778581d8064
ynna <kauannyferrerira454@gmail.com>
2025-03-03 18:38:14 -0300

    finalizado

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function calcular(tipo, valor) {
         }
 
         if (valor === '=') {
-            var valor_campo = eval(document.getElementById('resultado').value) //converter e atribiuir o resultado
+            var valor_campo = eval(document.getElementById('resultado').value) 
             
             document.getElementById('resultado').value = valor_campo
         }
